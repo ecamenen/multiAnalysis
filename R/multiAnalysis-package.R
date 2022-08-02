5066f564ad4f1ea9085fac24705c097f65e6120d
ecamenen <etienne.camenen@gmail.com>
2022-08-02 20:54:20 +0200

    Update libraries config

--- R/multiAnalysis-package.R
+++ R/multiAnalysis-package.R
@@ -1,5 +1,8 @@
-#' @import corrplot factoextra FactoMineR ggplot2 ggpubr ggrepel mixOmics
-#' @import plotly RGCCA rstatix tidyverse visNetwork
+#' @import corrplot factoextra FactoMineR ggplot2 ggpubr ggrepel
+#' @import RGCCA rstatix tidyverse visNetwork
 #' @importFrom magrittr %>%
+#' @rawNamespace import(mixOmics, except = print.rgcca)
+#' @rawNamespace import(modelr, except = c(print.permutation, bootstrap))
+#' @rawNamespace import(plotly, except = last_plot)
 #' @keywords internal
 "_PACKAGE"
