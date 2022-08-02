f01e8a28a43c23281e0b4e8460380d1a6b22c0f4
ecamenen <etienne.camenen@gmail.com>
2022-08-02 20:08:50 +0200

    Add libraries

--- R/multiAnalysis-package.R
+++ R/multiAnalysis-package.R
@@ -1,3 +1,5 @@
+#' @import corrplot factoextra FactoMineR ggplot2 ggpubr ggrepel mixOmics
+#' @import plotly RGCCA rstatix tidyverse visNetwork
 #' @importFrom magrittr %>%
 #' @keywords internal
 "_PACKAGE"
