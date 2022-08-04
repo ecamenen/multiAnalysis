d023b28718a47f7784893662ab27d25843989bdb
ecamenen <etienne.camenen@gmail.com>
2022-08-04 16:32:09 +0200

    Add doc for data

---
+++ R/blocks.R
@@ -0,0 +1,14 @@
+#' Blocks
+#'
+#' Tables of omics and clinic from the ImmunAID project (H2020) for
+#' Auto-Inflammatory Diseases (AID).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name blocks
+#' @usage data(blocks)
+"blocks"
+
+#' @rdname blocks
+#' @usage data(clinic)
+"clinic"
