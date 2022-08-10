5124f01e7a63754efe2d15dfd4f07477550066b6
ecamenen <etienne.camenen@gmail.com>
2022-08-10 09:45:37 +0200

    Add heatmap, colors

--- R/analyse_clustering.R
+++ R/analyse_clustering.R
@@ -1,18 +1,27 @@
 # Environment
 libs <- c(
     "cluster",
-    "NbClust"
+    "NbClust",
+    "dendextend",
+    "heatmaply"
 )
 block_name <- "blocks_clinic"
 hc_method <- "ward.D2"
 hc_metric <- "pearson"
 hc_index <- "silhouette"
 k <- 2
+colors_var <- c("indianred1", "darkseagreen")
+colors_ind <- c("blue", "white", "#cd5b45")
 source(file.path(golem::get_golem_wd(), "R", "set_analysis.R"))
 
 # Data preparation
 
-res_scaled <- scale(blocks[[1]])
+res_scaled_na0 <- scale(blocks[[1]])
+res_scaled0 <- res_scaled_na0
+res_scaled0[is.na(res_scaled0)] <- 0
+
+res_scaled_na <- normalize(blocks[[1]])
+res_scaled <- res_scaled_na
 res_scaled[is.na(res_scaled)] <- 0
 
 get_clust_tendency(res_scaled, n = nrow(res_scaled) - 1)
@@ -48,7 +57,7 @@ res_dist <- get_dist(res_scaled, stand = FALSE, method = hc_metric)
 
 fviz_dist(
     res_dist,
-    gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07")
+    gradient = list(low = colors_ind[1], mid = colors_ind[2], high = colors_ind[3])
 )
 
 # Clustering
@@ -71,14 +80,24 @@ res_clus <- eclust(
 )
 table(res_clus$cluster, disease)
 
+res_clus0 <- res_scaled0 %>%
+    get_dist(stand = FALSE, method = hc_metric) %>%
+    hclust(method = hc_method)
+table(cutree(res_clus0, k = k), disease)
+
+res_clus1 <-res_scaled %>%
+    t() %>%
+    get_dist(stand = FALSE, method = hc_metric) %>%
+    hclust(method = hc_method)
+
 # Visualisation
 
 fviz_dend(
     res_clus,
-    k = 2,
+    k = k,
     rect = TRUE,
     # rect_fill = TRUE,
-    # k_colors = c("#2E9FDF", "#00AFBB", "#E7B800"),
+    k_colors = colors_var[seq(k)],
     color_labels_by_k = TRUE,
 )
 
@@ -88,9 +107,50 @@ fviz_cluster(
     res_clus,
     repel = TRUE,
     show.clust.cent = TRUE,
-    palette = c("#00AFBB", "#FC4E07", "#62bd50"),
+    palette = colors_var,
     ggtheme = theme_minimal(),
-    main = "Factor map"
+    main = "Factor map",
+    ellipse.type = "norm"
+) + theme_classic()
+
+fviz_silhouette(res_clus, palette = colors_var) +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90))
+
+# Heatmap
+# gradient_col <- ggplot2::scale_color_gradientn(colours = colors, na.value = "black")
+
+colors_var <- c("yellow", "red")
+mycols <- c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07")
+row_dend <- res_clus %>%
+    as.dendrogram() %>%
+    set("branches_lwd", .1) %>%
+    set("branches_k_color", mycols[1:k], k = k)
+
+col_dend <- res_clus1 %>%
+    as.dendrogram() %>%
+    set("branches_lwd", .1) %>%
+    set("branches_k_color", mycols[1:k], k = k)
+
+heatmaply(
+    res_scaled,
+    k_col = k,
+    k_row = k,
+    colors = colors_ind,
+    distfun = hc_metric,
+    hclust_method = hc_method,
+    # Rowv = row_dend,
+    # Colv = col_dend,
+    # row_dend_left = TRUE
+    seriate = "none",
+    # col_side_colors = group_code,
+    row_side_colors = data.frame("Disease" = factor(disease, labels = c("Still", "Control")), check.names = FALSE),
+    row_side_palette = c("Still" = colors_var[1], "Control" = colors_var[2]),
+    # RowSideColors = factor(disease, labels = c("Still", "Control")),
+    plot_method = "plotly",
+    # key.title = "Disease"
+    # side_color_layers
 )
 
-fviz_silhouette(res_clus)
+# clinic_intersect %>% filter(str_detect(disease, "control")) %>%
+#     arrange(immun_aid_identifier)
