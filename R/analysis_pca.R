0354e3e8b4ab5a9e5dd6c8f10a41afac58b0b485
ecamenen <etienne.camenen@gmail.com>
2022-08-30 12:15:26 +0200

    Improve pca figures, add contribution, biplot

--- R/analysis_pca.R
+++ R/analysis_pca.R
@@ -1,35 +1,121 @@
 block_name <- "blocks_clinic"
 libs <- c(
     "FactoMineR",
-    "factoextra"
+    "factoextra",
+    "rstatix"
 )
 source(file.path(golem::get_golem_wd(), "R", "set_analysis.R"))
+colPers <- rev(colors_ind[3:2])
+blocks <- blocks[[1]]
 
-res_pca <- PCA(blocks[[1]], scale.unit = TRUE, ncp = 3, grap = FALSE) %>%
+theme_perso0 <- function(p) {
+    axis <- element_text(
+        face = "bold.italic",
+        size = 12
+    )
+    p +
+    theme(
+        axis.title =  axis,
+        plot.title = element_text(face = "bold", size = 14, hjust = 0.5),
+        legend.title = element_text(face = "italic")
+    )
+}
+
+theme_perso <- function(p) {
+    theme_perso0(
+        p +
+        theme_classic() +
+        geom_vline(
+            xintercept = 0,
+            col = "grey",
+            linetype = "dashed",
+            size = 0.5
+        ) +
+        geom_hline(
+            yintercept = 0,
+            col = "grey",
+            linetype = "dashed",
+            size = 0.5
+        )
+    )
+}
+
+res_pca <- PCA(blocks, scale.unit = TRUE, ncp = 3, grap = FALSE) %>%
     suppressWarnings()
+
+fviz_contrib(res_pca, choice = "ind", axes = 1, top = 50)
 tibble(res_pca$eig)
-fviz_eig(res_pca, addlabels = TRUE, ylim = c(0, 50))
-fviz_pca_var(
+fviz_eig(
     res_pca,
-    col.var = "contrib",
-    gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
-)
-fviz_contrib(res_pca, choice = "var", axes = 1, top = 20)
+    addlabels = TRUE,
+    geom = "bar",
+    hjust = 0.5,
+    ggtheme = theme_classic()
+) %>% theme_perso0()
 
-fviz_pca_ind(
-    res_pca,
-    col.ind = "cos2",
-    gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-    repel = TRUE
+# Variables
+fviz_contrib(res_pca, choice = "var", axes = 1, top = 50)
+get_ctr <- function(x, i = "var") {
+    x <- x[[i]]$contrib
+    sapply(
+        seq(ncol(x)),
+        function(i)
+            which((x[, i]  %>% sort(TRUE)) > (1 / nrow(x) * 100)))
+}
+(ctr <- get_ctr(res_pca))
+vars <- dimdesc(res_pca, 1:2)
+vars$Dim.1$quanti %>%
+    as.data.frame() %>%
+    adjust_pvalue("p.value") %>%
+    add_significance("p.value.adj")
+theme_perso(
+    fviz_pca_var(
+        res_pca,
+        col.var = "contrib",
+        repel = TRUE,
+        gradient.cols = colPers,
+        select.var = list(name = names(ctr[1:2]))
+    )
 )
 
-fviz_pca_ind(
-    res_pca,
-    geom.ind = "point",
-    col.ind = disease,
-    palette = c("#00AFBB", "#FC4E07", "#62bd50"),
-    addEllipses = TRUE,
-    legend.title = "Disease"
+# Individuals
+theme_perso(
+    fviz_pca_ind(
+        res_pca,
+        col.ind = "cos2",
+        gradient.cols = colors_ind,
+        repel = TRUE
+    )
+)
+
+theme_perso(
+    fviz_pca_ind(
+        res_pca,
+        geom.ind = "point",
+        col.ind = disease,
+        palette = colors_var,
+        addEllipses = TRUE,
+        legend.title = "Disease"
+    )
 )
 res_pca$ind
-fviz_contrib(res_pca, choice = "ind", axes = 1, top = 20)
+fviz_contrib(res_pca, choice = "ind", axes = 1, top = 50)
+corrplot(t(res_pca$ind$contrib), is.corr = FALSE, col = colorRampPalette(c(colPers, "black"))(100))
+corrplot(t(res_pca$var$contrib), is.corr = FALSE, col = colorRampPalette(c(colPers, "black"))(100))
+
+# Integrative
+theme_perso(
+    fviz_pca_biplot(
+        res_pca,
+        repel = TRUE,
+        col.var = "gray",
+        col.ind = disease,
+        gradient.cols = colPers,
+        # alpha.var = "cos2",
+        palette = colors_var[10:11],
+        legend.title = "Disease",
+        # pointsize = "cos2",
+        mean.point = FALSE
+        # select.var = list(name = names(unlist(ctr[1:2])))
+    )
+)
--- R/set_analysis.R
+++ R/set_analysis.R
@@ -12,7 +12,8 @@ RGCCA:::load_libraries(
     c(
         "RColorBrewer",
         libs,
-        "tidyverse" # ,
+        "tidyverse",
+        "dplyr"# ,
         # "vapoRwave"
     )
 )
@@ -20,13 +21,24 @@ RGCCA:::load_libraries(
 load(file.path(path, paste0(block_name, ".rda")))
 blocks <- get(block_name)
 
-load(file.path(path, "clinic.rda"))
+# to_remove <- c("1004", "1007", "1002", "25012", "1001", "1008", "1010")
+# i_row <- which(rownames(blocks[[1]]) %in% to_remove)
+# blocks <- lapply(blocks, function(i) i[-i_row, ])
+
+load(file.path(path, "clinic_transf.rda"))
 clinic_intersect <- filter(
-    clinic,
+    clinic_transf,
     str_detect(
         immun_aid_identifier,
         paste0("^", rownames(blocks[[1]]), "$", collapse = "|")
     )
-)
+) %>% arrange(immun_aid_identifier)
 disease <- clinic_intersect$disease %>%
     factor(., levels = unique(.))
+
+# colors_var <- c("indianred1", "darkseagreen", "steelblue")
+colors_var <- c(
+    brewer.pal(n = 9, name = "Pastel1"),
+    brewer.pal(n = 9, name = "Set1")
+)
+colors_ind <- c("blue", "white", "#cd5b45")
