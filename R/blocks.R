8bcbb61aab8cab8c46b8a24daad87f470b8be748
ecamenen <etienne.camenene@gmail.com>
2022-10-11 16:28:51 +0200

    Update data

--- R/blocks.R
+++ R/blocks.R
@@ -14,5 +14,37 @@
 "clinic"
 
 #' @rdname blocks
-#' @usage data(blocks_clinic)
-"blocks_clinic"
+#' @usage data(clinic_control_still)
+"clinic_control_still"
+
+#' @rdname blocks
+#' @usage data(clinic_qual)
+"clinic_qual"
+
+#' @rdname blocks
+#' @usage data(clinic_still_log)
+"clinic_still_log"
+
+#' @rdname blocks
+#' @usage data(clinic_still_log_proc)
+"clinic_still_log_proc"
+
+#' @rdname blocks
+#' @usage data(clinic_still_processed)
+"clinic_still_processed"
+
+#' @rdname blocks
+#' @usage data(clinic_still_raw)
+"clinic_still_raw"
+
+#' @rdname blocks
+#' @usage data(clinic_still_transformed)
+"clinic_still_transformed"
+
+#' @rdname blocks
+#' @usage data(clinic_transf)
+"clinic_transf"
+
+#' @rdname blocks
+#' @usage data(som_model)
+"som_model"
