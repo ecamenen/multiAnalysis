326d1ada664befe2a1f1db22b17fbabfe82f4168
ecamenen <etienne.camenene@gmail.com>
2022-10-11 16:26:02 +0200

    Add some exports

--- R/multiAnalysis-package.R
+++ R/multiAnalysis-package.R
@@ -1,12 +1,17 @@
 #' @import corrplot cluster dendextend dplyr factoextra FactoMineR ggplot2
 #' @import ggrepel ggstatsplot golem heatmaply NbClust pheatmap pvclust
-#' @import RColorBrewer RGCCA rstatix stringr tidyverse visNetwork
+#' @import RColorBrewer RGCCA stringr tidyverse visNetwork
 #' @importFrom graphics par text
 #' @importFrom stats as.dendrogram
 #' @importFrom magrittr %>%
 #' @rawNamespace import(ggpubr, except = rotate)
 #' @rawNamespace import(mixOmics, except = print.rgcca)
 #' @rawNamespace import(modelr, except = c(print.permutation, bootstrap))
-#' @rawNamespace import(plotly, except = last_plot)
+#' @rawNamespace import(plotly, except = c(last_plot, select))
+#' @rawNamespace import(rstatix, except = select)
+#' @importFrom grDevices as.raster dev.off heat.colors pdf tiff
+#' @importFrom graphics abline axis legend lines mtext points rasterImage rect title
+#' @importFrom stats cophenetic cor dist hclust kmeans na.omit rect.hclust var
+#' @importFrom utils data read.csv2
 #' @keywords internal
 "_PACKAGE"
